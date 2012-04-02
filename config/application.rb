476c25bf1ab317fadbe82c84bcd2816d14458135
Herby Raynaud <herby.raynaud@gmail.com>
2012-04-02 19:15:29 -0400

    Create landing controller and view

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,16 @@ module Homerefs
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.stylesheets  false
+      g.javascripts false
+      g.helper false
+      g.view_specs false
+      g.controller_specs false
+      g.routing_specs false
+      g.helper_specs false
+      g.fixture false
+    end
   end
 end
