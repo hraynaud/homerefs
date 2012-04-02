8d1ba58ba647f3e1c7819766c1ee52e17c2181f1
Herby Raynaud <herby.raynaud@gmail.com>
2012-04-02 16:36:00 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homerefs::Application.config.secret_token = '49edf539909671f588f93a01ac7f11f9896d5e8d1a636843af8123e8a9febbf55231ee5af4f9d38c5f1927c93c4cfa5bcaff17f543fac6fc64e5743f8fb7dc81'
