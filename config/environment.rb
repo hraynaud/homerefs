f75ab26ebf58744ce361cd2d34add01ccc75b854
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-04-05 20:04:18 -0400

    cleaning up env file

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-FACEBOOK_APP_ID = ENV['FACEBOOK_APP_ID']
-FACEBOOK_APP_SECRET = ENV['FACEBOOK_APP_SECRET']
 Homerefs::Application.initialize!
