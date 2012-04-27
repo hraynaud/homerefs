c7709f375981b53f2d9973e08272e1d7f119fa3d
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-04-27 16:07:16 -0400

    Adding images to admin view of reviews

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -8,5 +8,19 @@ ActiveAdmin.register Review do
     column :comment
     default_actions
   end
+
+  show do
+    attributes_table :id, :cleanliness, :pests, :sunlight, :convenience, :noise_level, :ceilings, :closet_space, :intercom_system, :temp_control, :appliances, :countertops, :floors, :bathrooms, :walls, :utilities, :neighbors, :laundry, :mass_transit, :neighborhood, :storage, :packages, :super
+    div do
+      review.building_images.each do |img|
+        div do
+          img.id
+        end
+        div do
+        image_tag img.image.url(:small)
+        end
+      end
+    end
+  end
 end
 
