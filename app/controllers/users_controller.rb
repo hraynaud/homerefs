4c082abf2b8c3eab7e795e9f61fbc37c077b7b9e
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-05-01 17:08:22 -0400

    Corrects user creation validations and changes order of birth year drop down

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,28 +5,35 @@ class UsersController < ApplicationController
 
   end
   def create
-    user = User.find_anywhere_by_email(params[:email] || session["omniauth"]["info"]["email"])
 
-    if user.nil?
-      user= User.new
-      user.dob = Date.new(params["date"]["year"].to_i, 1, 1) if params["date"]["year"].present?
-      user.gender = params["gender"] if params["gender"].present?
-      user.name = params["name"] if params["name"].present?
-      user.save
+    email = params[:email] || session["omniauth"]["info"]["email"]
+    @user = User.find_anywhere_by_email(email)
+
+    if @user.nil?
+      @user= User.new
+      @user.dob = Date.new(params["date"]["year"].to_i, 1, 1) if params["date"]["year"].present?
+      @user.gender = params["gender"] if params["gender"].present?
+      @user.name = params["name"] if params["name"].present?
+      if @user.valid?
+        @user.save
+      end
 
       if session["omniauth"] # Original login from fb
-        user.name = session["omniauth"]["info"]["name"]
-        UserToken.create(:provider => session["omniauth"]["provider"], :uid => session["omniauth"]["uid"], :email => session["omniauth"]["info"]["email"], :user_id => user.id )
+        @user.name = session["omniauth"]["info"]["name"]
+        UserToken.create(:provider => session["omniauth"]["provider"], :uid => session["omniauth"]["uid"], :email => session["omniauth"]["info"]["email"], :user_id => @user.id )
       end
     end
 
     unless session["omniauth"]
       id = Identity.find_by_email(params[:email])
       if id.nil?
-        id = Identity.new(:password=>params["password"],:password_confirmation=>params["password_confirmation"],:email=>params["email"])
-        if id.valid?
-          id.user_id = user.id
-          id.save
+        @identity = Identity.new(:name => params["name"],:password=>params["password"],:password_confirmation=>params["password_confirmation"],:email=>params["email"])
+          @identity.user_id = @user.id
+        if @identity.valid?
+          @identity.save
+        else
+          render new_identity_path
+          return
         end
       else
         flash.now[:error] = "Idenity already exists"
@@ -34,7 +41,7 @@ class UsersController < ApplicationController
         return
       end
     end
-    session[:user_id] = user.id
+    session[:user_id] = @user.id
     session["omniauth"] = nil
     redirect_to session["user_return_to"]
   end
--- app/models/identity.rb
+++ app/models/identity.rb
@@ -2,6 +2,7 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
   belongs_to :user
   validates_uniqueness_of :email
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates :user_id, :presence => true
 
 
 end
