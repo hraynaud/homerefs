ecde8ab840199def2f3529cb5a40e838f0699782
Herby Raynaud <herby.raynaud@gmail.com>
2012-05-05 23:01:07 -0400

    Added require field avgs to environment to test for heroku errors

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+require File.expand_path('../../lib/field_avgs', __FILE__)
 # Initialize the rails application
 Homerefs::Application.initialize!
 
