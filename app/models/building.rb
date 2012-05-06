806a9522abf525aea71f3ed57835c3fac2f41ad8
Herby Raynaud <herby.raynaud@gmail.com>
2012-05-05 23:36:06 -0400

    commented out ReviewMetadata

--- app/models/building.rb
+++ app/models/building.rb
@@ -7,15 +7,15 @@ class Building < ActiveRecord::Base
 
     module ClassMethods
       def define_field_avgs
-        ReviewMetadata.all_fields.each do |field|
-          define_method("#{field}_avg".to_sym) {
-            if(reviews != [])
-              20 * Review.where(:building_id => self.id).average(field)
-            else
-              0
-            end
-          }
-        end
+        # ReviewMetadata.all_fields.each do |field|
+          # define_method("#{field}_avg".to_sym) {
+            # if(reviews != [])
+              # 20 * Review.where(:building_id => self.id).average(field)
+            # else
+              # 0
+            # end
+          # }
+        # end
       end
     end
   end
