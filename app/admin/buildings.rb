e608446ec712a3417acc0bf0d78e5e65563f48a4
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-05-08 13:30:14 -0400

    cleaning up active admin for old migrations

--- app/admin/buildings.rb
+++ app/admin/buildings.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Building do
   show do
-    attributes_table :id, :address, :doorman, :elevator, :super, :coop, :construction_date, :city, :zipcode, :avg_score, :neighborhood, :default_image_id
+    attributes_table :id, :address, :doorman, :elevator, :super, :coop, :city, :zipcode, :neighborhood, :default_image_id, :construction_type
   end
 
   form :partial => "form"
