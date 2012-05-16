2b6b7212857248c39099016e20c6b22d73084e97
Herby Raynaud <herby.raynaud@gmail.com>
2012-05-16 11:37:36 -0400

    fixed bug with first review value not changing

--- app/observers/review_observer.rb
+++ app/observers/review_observer.rb
@@ -3,15 +3,15 @@ class ReviewObserver < ActiveRecord::Observer
   def after_save(review)
     bldg = review.building
 
-    if bldg.score.nil? or bldg.score==0
+    if bldg.score.nil? or bldg.score==0.0 or bldg.reviews_count ==0
       bldg.score = review.score
     else
       total = bldg.score * (bldg.reviews_count-1)
       total += review.score
       bldg.score = total/bldg.reviews_count
-      bldg.save
     end
 
+      bldg.save
   end
 
 end
