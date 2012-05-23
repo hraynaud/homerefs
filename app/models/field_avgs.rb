8cb408c1d400442b4f56cb83c5452accb3cddc14
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-05-22 23:27:20 -0400

    re-enabling field_avgs for staging

--- app/models/field_avgs.rb
+++ app/models/field_avgs.rb
@@ -6,15 +6,15 @@ module FieldAvgs
 
   module ClassMethods
     def define_field_avgs
-      # ReviewMetadata.all_fields.each do |field|
-        # define_method("#{field}_avg".to_sym) {
-          # if(reviews != [])
-           # 20 * Review.where(:building_id => self.id).average(field)
-          # else
-            # 0
-          # end
-        # }
-      # end
+      ReviewMetadata.all_fields.each do |field|
+        define_method("#{field}_avg".to_sym) {
+          if(reviews != [])
+           20 * Review.where(:building_id => self.id).average(field)
+          else
+            0
+          end
+        }
+      end
     end
   end
 end
