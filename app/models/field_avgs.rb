0d574bf406dfce8049e5aa628ec0c2a1ac1df6f5
Gregg Rubin <gregg.m.rubin@gmail.com>
2012-05-22 23:21:38 -0400

    TTemp disable define_field_avs

---
+++ app/models/field_avgs.rb
@@ -0,0 +1,20 @@
+module FieldAvgs
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def define_field_avgs
+      # ReviewMetadata.all_fields.each do |field|
+        # define_method("#{field}_avg".to_sym) {
+          # if(reviews != [])
+           # 20 * Review.where(:building_id => self.id).average(field)
+          # else
+            # 0
+          # end
+        # }
+      # end
+    end
+  end
+end
