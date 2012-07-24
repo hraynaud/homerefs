360f97911bead56ecb35d4c44333fae879edd2be
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-24 00:04:56 -0400

    Added api keys and data to environment vars

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+S3_BUCKET_NAME = ENV['S3_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = ENV['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = ENV['AWS_SECRET_ACCESS_KEY']
 # Initialize the rails application
 Homerefs::Application.initialize!
 
