73847593e9142b8c53b06e66f721de6ee3d18966
Herby Raynaud <herby.raynaud@gmail.com>
2012-07-23 20:47:24 -0400

    Formatted layout of buildings admin page

--- app/admin/buildings.rb
+++ app/admin/buildings.rb
@@ -1,7 +1,56 @@
 ActiveAdmin.register Building do
-  show do
-    attributes_table :id, :address, :doorman, :elevator, :super, :coop, :city, :zipcode, :neighborhood, :default_image_id, :construction_type
+
+  index do
+
+    column :address
+    column :neighborhood
+    column :doorman do |val|
+      val ? "Yes" : "No"
+    end
+
+    column :elevator do |val|
+      val ? "Yes" : "No"
+    end
+
+    column :super do |val|
+      val ? "Yes" : "No"
+    end
+
+    column :coop do |val|
+      val ? "Yes" : "No"
+    end
+    column :construction
+    column :zipcode
+    default_actions
   end
 
+
+  show :title => :address do |building|
+    attributes_table do
+      row :neighborhood
+      row :city
+      row :construction_type
+      row :doorman do
+        building.doorman ? "Yes" : "No"
+      end
+      row :elevator do
+        building.elevator ? "Yes" : "No"
+      end
+      row :super do
+        building.super ? "Yes" : "No"
+      end
+
+      row :coop do
+        building.coop ? "Yes" : "No"
+      end
+
+
+      row :zipcode
+      row :id
+      row :default_image do
+         image_tag(building.default_building_image.image.url)
+      end
+    end
+  end
   form :partial => "form"
 end
