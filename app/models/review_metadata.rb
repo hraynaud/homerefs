1c96f28df81ac8e0a88b3ce60c489f3106add29d
Herby Raynaud <herby.raynaud@gmail.com>
2012-08-02 12:07:25 -0400

    Added check to make sure the table exists at the time of this call otherwise return an empty array

--- app/models/review_metadata.rb
+++ app/models/review_metadata.rb
@@ -1,5 +1,11 @@
 class ReviewMetadata < ActiveRecord::Base
   def self.all_fields
-    ReviewMetadata.order("position asc").all.map(&:field_name)
+    # NOTE do a check on existence of this table otherwise when rails loads the environment it triggers a race condiion while loading of  active admin gem which calls this method
+    # and prevents even the migrations from running. -
+    if table_exists?
+      self.order("position asc").all.map(&:field_name)
+    else
+      []
+    end
   end
 end
