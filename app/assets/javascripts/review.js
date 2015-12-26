1838f161555fc1e5763c1bc2df7f2aacd166bbcc
hraynaud <herby.raynaud@gmail.com>
2015-12-26 07:29:46 -0500

    Cleans up review submission js

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -2,9 +2,6 @@ var HomeRefs = window.HomeRefs || {};
 
 HomeRefs.Reviewer = (function(){
 
-  var fieldArray ;
-  var fieldArrayIds = [];
-
   function updateValues(){
     $(".ui-slider").each(function(){
       var sliderId = $(this).attr("id")
@@ -15,42 +12,52 @@ HomeRefs.Reviewer = (function(){
 
   };
 
-  function submitWithAjax() {
-    updateValues();
-    $.post(this.action, $(this.serialize(), null, "script"));
-    return false;
+
+  function initSlider(){
+    var allFields= $("#review_form").data("allfields")
+      , id
+      , hiddenId
+    ;
+
+    allFields.forEach(function(item){
+      id = "#" + item;  
+      hiddenId = "#_" + item;
+
+      $(id).slider({
+        range: "min",
+        min: 1,
+        max: 5,
+        value: $(hiddenId).val() || 3
+      });
+
+    });
+  }
+
+  function initFormSubmit(){
+
+    $("#review_form").on('submit', function(event){
+      event.preventDefault();
+      updateValues();
+      this.submit();  
+    });
+  }
+
+  function initPageElements(){
+    $("ul.bldg-stats.row").tooltip();
+    $("li[rel=popover]").popover({placement: 'left'});
   }
 
   function init(){
 
     if($("#review_form").length > 0){
+      initSlider();
+      initFormSubmit();
 
-      fieldArray= $("#review_form").data("allfields")
-      fieldArray.forEach(function(item){
-        fieldArrayIds.push("#"+item);
-      });
-
-      fieldArrayIds.forEach(function(item){
-        hiddenItem = "#_" + item.substring(1);
-        $(item).slider({
-          range: "min",
-          min: 1,
-          max: 5,
-          value: $(hiddenItem).val() || 3
-        });
-      });
-
-      $('.slider').slider();
-
-      $("#review_form").bind('submit', submitWithAjax);
-
+      initPageElements();
       $("ul.bldg-stats.row").click(function (el){
         document.location.href ='buildings/' + $(this).attr("data-bldg-id");
       });
 
-      $("ul.bldg-stats.row").tooltip();
-
-      $("li[rel=popover]").popover({placement: 'left'});
     }
   }
 
@@ -62,13 +69,8 @@ HomeRefs.Reviewer = (function(){
 }());
 
 
-
-
-
-
-
 $(function() { 
 
+  HomeRefs.Reviewer.init();
 });
 
-HomeRefs.Reviewer.init();
