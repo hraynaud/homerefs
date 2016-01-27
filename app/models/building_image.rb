2f9d567cebff88580f302ad6f821b5cb1a64c91c
hraynaud <herby.raynaud@gmail.com>
2016-01-26 22:54:20 -0500

    Saves images to local machine in dev

--- app/models/building_image.rb
+++ app/models/building_image.rb
@@ -3,14 +3,18 @@ class BuildingImage < ActiveRecord::Base
   belongs_to :review
   belongs_to :building
 
+  if Rails.env.production?
+    PAPERCLIP_OPTS = { :styles => { :small => "200x200#", :index => "119x111#"},
+                       :storage => :s3,
+                       :bucket => S3_BUCKET_NAME,
+                       :s3_credentials => {
+                         :access_key_id => AWS_ACCESS_KEY_ID,
+                         :secret_access_key => AWS_SECRET_ACCESS_KEY
+                       }}
 
-  PAPERCLIP_OPTS = { :styles => { :small => "200x200#", :index => "119x111#"},
-        :storage => :s3,
-        :bucket => S3_BUCKET_NAME,
-        :s3_credentials => {
-          :access_key_id => AWS_ACCESS_KEY_ID,
-          :secret_access_key => AWS_SECRET_ACCESS_KEY
-        }}
+  else Rails.env.development?
+    PAPERCLIP_OPTS = { :styles => { :small => "200x200#", :index => "119x111#"} }
+  end
 
   has_attached_file :image, PAPERCLIP_OPTS
 
